81d8cf359d9efd013279eeda3423a95b1871ce5e
Bernhard <b_walter@arcor.de>
2023-01-28 13:20:47 +0100

    new and changed tests

--- tests/test_cadquery_sketch.py
+++ tests/test_cadquery_sketch.py
@@ -17,45 +17,45 @@ result = (
     .close()
     .arc((0.6, 0.6), 0.4, 0.0, 360.0)
     .assemble(tag="face")
-    .edges("%LINE", tag="face")
-    .vertices()
-    .chamfer(0.2)
-    .reset()
+    # .edges("%LINE", tag="face")
+    # .vertices()
+    # .chamfer(0.2)
+    # .reset()
 )
 
 show(result)
 # %%
 
-result = (cq
-    .Sketch()
+result = (
+    cq.Sketch()
     .trapezoid(4, 3, 90)
     .vertices()
-    .circle(0.5, mode='s')
+    .circle(0.5, mode="s")
     .reset()
     .vertices()
     .fillet(0.25)
     .reset()
     .rarray(0.6, 1, 5, 1)
-    .slot(01.5, 0.4, mode='s', angle=90)
+    .slot(01.5, 0.4, mode="s", angle=90)
     .reset()
 )
 show(result)
 
 # %%
 
-result = (cq
-    .Workplane()
-    .transformed((0,90,90),(2,0,0))
+result = (
+    cq.Workplane()
+    .transformed((0, 90, 90), (2, 0, 0))
     .sketch()
     .trapezoid(4, 3, 90)
     .vertices()
-    .circle(0.5, mode='s')
+    .circle(0.5, mode="s")
     .reset()
     .vertices()
     .fillet(0.25)
     .reset()
     .rarray(0.6, 1, 5, 1)
-    .slot(01.5, 0.4, mode='s', angle=90)
+    .slot(01.5, 0.4, mode="s", angle=90)
     .reset()
     .finalize()
 )
---
+++ tests/test_partgroup.py
@@ -0,0 +1,34 @@
+import cadquery as cq
+from cq_vscode import show
+from jupyter_cadquery import Part, PartGroup
+
+
+def ring(inner_radius, outer_radius, width):
+    ring = (
+        cq.Workplane(origin=(0, 0, -width / 2))
+        .circle(outer_radius)
+        .circle(inner_radius)
+        .extrude(width)
+    )
+    return ring
+
+
+tol = 0.05
+ball_diam = 5
+
+r1, r2, r3, r4 = 4, 6, 8, 10
+r5 = (r3 + r2) / 2
+inner_ring = ring(r1, r2, ball_diam)
+outer_ring = ring(r3, r4, ball_diam)
+
+torus = cq.CQ(cq.Solid.makeTorus(r5, ball_diam / 2 + tol))
+ball = cq.Workplane().sphere(ball_diam / 2)
+
+inner = inner_ring.cut(torus, clean=False)
+outer = outer_ring.cut(torus, clean=False)
+
+show(
+    PartGroup(
+        [Part(ball, "ball"), Part(inner, "inner"), Part(outer, "outer")], "bearing"
+    )
+)
