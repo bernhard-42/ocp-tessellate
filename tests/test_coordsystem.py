cd21cbd4feccfc463fbd02d24853eb1a84f53c19
Bernhard <b_walter@arcor.de>
2023-01-30 21:09:17 +0100

    added name parameter

--- tests/test_coordsystem.py
+++ tests/test_coordsystem.py
@@ -1,6 +1,8 @@
 from cq_vscode import show
 from ocp_tessellate.cad_objects import CoordSystem
 
-c = CoordSystem((1, 1, 1), (1, 0, 0), (0, 1, 0), (0, 0, 1), 1)
+c = CoordSystem("xyz", (1, 1, 1), (1, 0, 0), (0, 1, 0), (0, 0, 1), 1)
 
 show(c)
+
+# %%
