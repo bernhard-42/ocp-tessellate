bc876f45ada666620b6d8a5e6a6f4b66e7ce04b3
Bernhard <b_walter@arcor.de>
2023-04-22 18:08:25 +0200

    Latest release: 1.0.0

---
+++ tests/test_parallel.py
@@ -0,0 +1,29 @@
+import os
+
+os.environ["OCP_CACHE_SIZE_MB"] = "1024"
+
+import cadquery as cq
+from build123d import *
+
+from ocp_tessellate.stepreader import StepReader
+import time
+
+# %%
+
+reader = StepReader()
+reader.load("/tmp/RC_Buggy_2_front_suspension.stp")
+rc = reader.to_cadquery()
+
+# %%
+
+t = time.time()
+show(rc, up="Y", parallel=True, timeit=False)
+print("\n", time.time() - t)
+
+# %%
+
+t = time.time()
+show(rc, up="Y", parallel=True, timeit=False)
+print("\n", time.time() - t)
+
+# %%
