5f7e395a3b76ad5d1da611894bd7d9bd1c2dbde8
Bernhard <b_walter@arcor.de>
2023-06-24 15:03:16 +0200

    Latest release: 1.1.1

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ LONG_DESCRIPTION = (
 
 setup_args = {
     "name": "ocp_tessellate",
-    "version": "1.1.0",
+    "version": "1.1.1",
     "description": "Tessellate OCP objects",
     "long_description": LONG_DESCRIPTION,
     "include_package_data": True,
