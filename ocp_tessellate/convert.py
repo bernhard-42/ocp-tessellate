7983fe43cd88b59236575683ad8ce6255b42d189
Bernhard <b_walter@arcor.de>
2024-05-19 14:16:10 +0200

    support parent

--- ocp_tessellate/convert.py
+++ ocp_tessellate/convert.py
@@ -228,6 +228,38 @@ class OcpConverter:
                 width=LINE_WIDTH if kind == "edge" else POINT_SIZE,
             )
 
+    def handle_parent(self, cad_obj, obj_name, rgba_color, level):
+        parent = None
+        if hasattr(cad_obj, "parent"):
+            parent = cad_obj.parent
+            topo = False
+        elif hasattr(cad_obj, "topo_parent"):
+            parent = cad_obj.topo_parent
+            topo = True
+        elif (
+            isinstance(cad_obj, Iterable)
+            and len(cad_obj) > 0
+            and hasattr(cad_obj[0], "topo_parent")
+        ):
+            parent = cad_obj[0].topo_parent
+            topo = True
+
+        ind = 0
+        parents = []
+        while parent is not None:
+            pname = "parent" if ind == 0 else f"parent({ind})"
+            p = self.to_ocp(parent, names=[pname], colors=[rgba_color], level=level + 1)
+            p = p.objects[0]
+            if p.kind == "solid":
+                p.state_faces = 0
+            elif p.kind == "face":
+                p.state_edges = 0
+            parents.insert(0, p)
+            parent = parent.topo_parent if topo else None
+            ind -= 1
+
+        return parents
+
     def handle_list_tuple(
         self, cad_obj, obj_name, rgba_color, sketch_local, helper_scale, level
     ):
@@ -328,14 +360,16 @@ class OcpConverter:
             ocp_obj.add(sub_obj)
         return ocp_obj
 
-    def handle_build123d_shapelist(
+    def handle_shape_list(
         self,
         cad_obj,
         obj_name,
         rgba_color,
-        sketch_local,
+        show_parent,
         level,
     ):
+        parent_obj = cad_obj
+
         if is_build123d_shapelist(cad_obj):
             _debug(level, "handle_shapelist (build123d ShapeList)", obj_name)
             name = "ShapeList"
@@ -376,7 +410,11 @@ class OcpConverter:
                 color=get_color_for_object(objs[0], rgba_color),
                 width=LINE_WIDTH if kind == "edge" else POINT_SIZE,
             )
-        return ocp_obj
+        if show_parent:
+            parents = self.handle_parent(parent_obj, obj_name, rgba_color, level)
+            return OcpGroup(parents + [ocp_obj], name=ocp_obj.name)
+        else:
+            return ocp_obj
 
     def handle_build123d_builder(
         self, cad_obj, obj_name, rgba_color, sketch_local, level
@@ -744,7 +782,7 @@ class OcpConverter:
                 is_cadquery(cad_obj) and not is_cadquery_empty_workplane(cad_obj)
             ):
                 ocp_obj = self.handle_build123d_shapelist(
-                    cad_obj, obj_name, rgba_color, sketch_local, level
+                    cad_obj, obj_name, rgba_color, show_parent, level
                 )
 
             # build123d BuildPart, BuildSketch, BuildLine
