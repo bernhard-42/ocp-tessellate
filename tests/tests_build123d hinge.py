19dd6e8bcd0b950c5803135dbfaea994d9ff0907
Bernhard <b_walter@arcor.de>
2025-01-27 22:39:03 +0100

    copy latest version of the example

--- tests/tests_build123d hinge.py
+++ tests/tests_build123d hinge.py
@@ -1,15 +1,26 @@
 # %%
 from build123d import *
-from ocp_vscode import set_defaults, show, show_object
+from ocp_vscode import *
+
 
 # %%
-set_defaults(render_joints=True)
-
-# %%
-
-
-# [Hinge Class]
 class Hinge(Compound):
+    """Hinge
+
+    Half a simple hinge with several joints. The joints are:
+    - "leaf": RigidJoint where hinge attaches to object
+    - "hinge_axis": RigidJoint (inner) or RevoluteJoint (outer)
+    - "hole0", "hole1", "hole2": CylindricalJoints for attachment screws
+
+    Args:
+        width (float): width of one leaf
+        length (float): hinge length
+        barrel_diameter (float): size of hinge pin barrel
+        thickness (float): hinge leaf thickness
+        pin_diameter (float): hinge pin diameter
+        inner (bool, optional): inner or outer half of hinge . Defaults to True.
+    """
+
     def __init__(
         self,
         width: float,
@@ -21,7 +32,7 @@ class Hinge(Compound):
     ):
         # The profile of the hinge used to create the tabs
         with BuildPart() as hinge_profile:
-            with BuildSketch() as sk:
+            with BuildSketch():
                 for i, loc in enumerate(
                     GridLocations(0, length / 5, 1, 5, align=(Align.MIN, Align.MIN))
                 ):
@@ -55,8 +66,8 @@ class Hinge(Compound):
 
         # Either the external and internal leaf with joints
         with BuildPart() as leaf_builder:
-            with BuildSketch() as sk:
-                with BuildLine() as ln:
+            with BuildSketch():
+                with BuildLine():
                     l1 = Line((0, 0), (width - barrel_diameter / 2, 0))
                     l2 = RadiusArc(
                         l1 @ 1,
@@ -91,12 +102,10 @@ class Hinge(Compound):
                 with Locations(pin_center.locations[0]):
                     add(pin.part)
 
-            # [Create the Joints]
             #
             # Leaf attachment
             RigidJoint(
                 label="leaf",
-                to_part=leaf_builder.part,
                 joint_location=Location(
                     (width - barrel_diameter, 0, length / 2), (90, 0, 0)
                 ),
@@ -105,35 +114,30 @@ class Hinge(Compound):
             if inner:
                 RigidJoint(
                     "hinge_axis",
-                    leaf_builder.part,
-                    Location((width - barrel_diameter / 2, barrel_diameter / 2, 0)),
+                    joint_location=Location(
+                        (width - barrel_diameter / 2, barrel_diameter / 2, 0)
+                    ),
                 )
             else:
                 RevoluteJoint(
                     "hinge_axis",
-                    leaf_builder.part,
                     axis=Axis(
                         (width - barrel_diameter / 2, barrel_diameter / 2, 0), (0, 0, 1)
                     ),
                     angular_range=(90, 270),
                 )
-            # [Fastener holes]
             hole_locations = [hole.location for hole in holes]
             for hole, hole_location in enumerate(hole_locations):
                 CylindricalJoint(
                     label="hole" + str(hole),
-                    to_part=leaf_builder.part,
                     axis=hole_location.to_axis(),
                     linear_range=(-2 * CM, 2 * CM),
                     angular_range=(0, 360),
                 )
-            # [End Fastener holes]
 
         super().__init__(leaf_builder.part.wrapped, joints=leaf_builder.part.joints)
-        # [Hinge Class]
 
 
-# [Create instances of the two leaves of the hinge]
 hinge_inner = Hinge(
     width=5 * CM,
     length=12 * CM,
@@ -150,9 +154,6 @@ hinge_outer = Hinge(
     inner=False,
 )
 
-# %%
-
-# [Create the box with a RigidJoint to mount the hinge]
 with BuildPart() as box_builder:
     box = Box(30 * CM, 30 * CM, 10 * CM)
     offset(amount=-1 * CM, openings=box_builder.faces().sort_by(Axis.Z)[-1])
@@ -167,16 +168,11 @@ with BuildPart() as box_builder:
             Hole(3 * MM, 1 * CM)
     RigidJoint(
         "hinge_attachment",
-        box_builder.part,
-        Location((-15 * CM, 0, 4 * CM), (180, 90, 0)),
+        joint_location=Location((-15 * CM, 0, 4 * CM), (180, 90, 0)),
     )
 
-# [Demonstrate that objects with Joints can be moved and the joints follow]
 box = box_builder.part.moved(Location((0, 0, 5 * CM)))
 
-# %%
-
-# [The lid with a RigidJoint for the hinge]
 with BuildPart() as lid_builder:
     Box(30 * CM, 30 * CM, 1 * CM, align=(Align.MIN, Align.CENTER, Align.MIN))
     with Locations((2 * CM, 0, 0)):
@@ -184,75 +180,41 @@ with BuildPart() as lid_builder:
             Hole(3 * MM, 1 * CM)
     RigidJoint(
         "hinge_attachment",
-        lid_builder.part,
-        Location((0, 0, 0), (0, 0, 180)),
+        joint_location=Location((0, 0, 0), (0, 0, 180)),
     )
 lid = lid_builder.part
 
-# %%
-
-# [A screw to attach the hinge to the box]
 m6_screw = import_step("tests/M6-1x12-countersunk-screw.step")
 m6_joint = RigidJoint("head", m6_screw, Location((0, 0, 0), (0, 0, 0)))
-# [End of screw creation]
 
-# [Connect Box to Outer Hinge]
 box.joints["hinge_attachment"].connect_to(hinge_outer.joints["leaf"])
 
-# [Connect Hinge Leaves]
 hinge_outer.joints["hinge_axis"].connect_to(hinge_inner.joints["hinge_axis"], angle=120)
 
-# [Connect Hinge to Lid]
 hinge_inner.joints["leaf"].connect_to(lid.joints["hinge_attachment"])
 
-# [Connect Screw to Hole]
 hinge_outer.joints["hole2"].connect_to(m6_joint, position=5 * MM, angle=30)
-# [Connect Screw to Hole]
 
-# [Add labels]
-box.label = "box"
-lid.label = "lid"
-hinge_outer.label = "outer hinge"
-hinge_inner.label = "inner hinge"
-m6_screw.label = "M6 screw"
-
+show(box, lid, hinge_outer, hinge_inner, m6_screw, render_joints=True)
+# %%
 # [Create assembly]
 box_assembly = Compound(label="assembly", children=[box, lid, hinge_inner, hinge_outer])
 # [Display assembly]
 print(box_assembly.show_topology())
 
+show(box_assembly, render_joints=True)
+
+# %%
 # [Add to the assembly by assigning the parent attribute of an object]
 m6_screw.parent = box_assembly
 print(box_assembly.show_topology())
-
+# %%
 # [Check that the components in the assembly don't intersect]
 child_intersect, children, volume = box_assembly.do_children_intersect(
     include_parent=False
 )
 print(f"do children intersect: {child_intersect}")
+# %%
 if child_intersect:
     print(f"{children} by {volume:0.3f} mm^3")
 show(box_assembly, render_joints=True)
-
-# %%
-if "show_object" in locals():
-    # show_object(box, name="box", options={"alpha": 0.8})
-    # show_object(hinge_outer, name="hinge_outer")
-    # show_object(lid, name="lid")
-    # show_object(hinge_inner, name="hinge_inner")
-
-    for hole in [0, 1, 2]:
-        show_object(
-            hinge_inner.joints["hole" + str(hole)].symbol,
-            name="hinge_inner hole " + str(hole),
-            clear=False,
-        )
-        show_object(
-            hinge_outer.joints["hole" + str(hole)].symbol,
-            name="hinge_outer hole " + str(hole),
-        )
-    # show_object(m6_screw, name="m6 screw")
-    # show_object(m6_joint.symbol, name="m6 screw symbol")
-    show_object(box_assembly, name="box assembly")
-
-# %%
